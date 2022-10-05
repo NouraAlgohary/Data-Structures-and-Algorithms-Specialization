28836d5db9b9606d4b85eb9f9218b912bf99726e
Noura Algohary <103903785+NouraAlgohary@users.noreply.github.com>
2022-10-05 10:59:30 +0200

    Update W1 - Maximum Pairwise Product.cpp

--- Algorithmic Toolbox/W1 - Maximum Pairwise Product.cpp
+++ Algorithmic Toolbox/W1 - Maximum Pairwise Product.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int elementWise(int n, int arr[])
-{
-    int mx = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            if (arr[i] * arr[j] > mx) mx = arr[i] * arr[j];
-        }
-    }
-    return mx;
-}
 long long firstTwo(int n, int arr[])
 {
     long long first = 0, second = 0;
@@ -40,8 +28,6 @@ int main()
     cin >> n;
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-   // cout << "element pairwise product = " << elementWise(n, arr) << endl;
-    //cout << "two numbers product = " << firstTwo(n, arr) << endl;
-    cout<< firstTwo(n, arr) << endl;
     
+    cout<< firstTwo(n, arr) << endl;
 }
